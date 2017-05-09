58df650db4dee866545b786602fc3604439f0161
everardosifuentes <everardo.sifuentes@sbcglobal.net>
2017-05-09 18:04:37 -0500

    added code to do even numbers from 2 to 100 by 2s

--- src/controlFlowExercises.java
+++ src/controlFlowExercises.java
@@ -4,15 +4,18 @@
 public class controlFlowExercises {
     public static void main(String[] args) {
 
-        int i = 5;
-        while(i <= 15) {
-            System.out.println("The value of i is: " + i);
-            i++;
-        }
+//        int i = 5;
+//        while(i <= 15) {
+//            System.out.println("The value of i is: " + i);
+//            i++;
+//        }
 
 
-
-
+        int i = 2;
+        do {
+            System.out.println("the numbers are: " + i);
+            i = i + 2;
+        }while (i<=100);
 
 
 
