87fada22a95e9534c2e5a6fcaed9890d1dfd9da5
everardosifuentes <everardo.sifuentes@sbcglobal.net>
2017-05-10 10:09:02 -0500

    added square and cube program

--- src/controlFlowExercises.java
+++ src/controlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by everardosifuentes on 5/9/17.
  */
@@ -25,28 +27,67 @@ public class controlFlowExercises {
 //          } while (i >= -10);
 
 
-//            int i = 2;
-//            do {
-//                System.out.println("the numbers are: " + i);
-//                i = i * 2;
-//            } while (i<=100000);
+//        long i = 2;
+//        do {
+//            System.out.println("the numbers are: " + i);
+//            i = i * i;
+//        } while (i <= 100000);
 
 
-for(int i = 1; i <= 100; i++) {
+//for(int i = 1; i <= 100; i++) {
+//
+//    if (i % 3 == 0 && i % 5 == 0) {
+//        System.out.println("FizzBuzz");
+//    } else if (i % 3 == 0) {
+//        System.out.println("Fizz");
+//    } else if (i % 5 == 0) {
+//        System.out.println("Buzz");
+//    } else {
+//        System.out.println(i);
+//    }
+//
+//}
 
-    if (i % 3 == 0 && i % 5 == 0) {
-        System.out.println("FizzBuzz");
-    } else if (i % 3 == 0) {
-        System.out.println("Fizz");
-    } else if (i % 5 == 0) {
-        System.out.println("Buzz");
-    } else {
-        System.out.println(i);
-    }
 
-}
 
+        String yesNo;
+        Scanner sc = new Scanner(System.in);
+
+
+        do {
+            System.out.println("What number would you like to go up to:  ");
+
+            int num = sc.nextInt();
+
+            System.out.println("Here is your table!");
+            System.out.println();
+            System.out.println("number    |    squared    |    cubed");
+            System.out.println("------    |    -------    |    -----");
+
+            for (int i = 1; i <= num; i++) {
+                System.out.println("   " + i + "      |      " + square(i) + "        |     " + cube(i));
+            }
+
+            sc.nextLine();
+            System.out.println("Would you like to continue:  ");
+            yesNo = sc.nextLine();
+
+        }while (yesNo.equalsIgnoreCase("Y"));
 
     } // end main
+
+    public static int square(int num)
+    {
+        int sNum = num * num;
+        return sNum;
+
+    }
+
+    public static int cube(int num)
+    {
+        int cNum = num * num * num;
+        return cNum;
+
+    }
 
 }
