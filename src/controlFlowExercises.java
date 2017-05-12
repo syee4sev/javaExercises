121f58a361af92c0d9b04d51661642833ecb7de5
everardosifuentes <everardo.sifuentes@sbcglobal.net>
2017-05-12 16:40:18 -0500

    factorial and method projects

--- src/controlFlowExercises.java
+++ src/controlFlowExercises.java
@@ -34,19 +34,19 @@ public class controlFlowExercises {
 //        } while (i <= 100000);
 
 
-//for(int i = 1; i <= 100; i++) {
-//
-//    if (i % 3 == 0 && i % 5 == 0) {
-//        System.out.println("FizzBuzz");
-//    } else if (i % 3 == 0) {
-//        System.out.println("Fizz");
-//    } else if (i % 5 == 0) {
-//        System.out.println("Buzz");
-//    } else {
-//        System.out.println(i);
-//    }
-//
-//}
+for(int i = 1; i <= 100; i++) {
+
+    if (i % 3 == 0 && i % 5 == 0) {
+        System.out.println("FizzBuzz");
+    } else if (i % 3 == 0) {
+        System.out.println("Fizz");
+    } else if (i % 5 == 0) {
+        System.out.println("Buzz");
+    } else {
+        System.out.println(i);
+    }
+
+}
 
 
 //        String yesNo;
@@ -91,36 +91,75 @@ public class controlFlowExercises {
 
 
 
-String yesNo;
-Scanner sc = new Scanner(System.in);
 
 
-do {
-    System.out.println("Please enter your grade:  ");
-    int grade = sc.nextInt();
+//        String yesNo;
+//        Scanner sc = new Scanner(System.in);
+//
+//        do {
+//            System.out.println("What number would you like to go up to:  ");
+//
+//            int num = sc.nextInt();
+//
+//            System.out.println("Here is your table!");
+//            System.out.println();
+//            System.out.println("number    |    squared    |    cubed");
+//            System.out.println("------    |    -------    |    -----");
+//
+//            for (int i = 1; i <= num; i++) {
+//                System.out.println("   " + i + "      |      " + (int) Math.pow(i, 2) + "        |     " + (int) Math.pow(i, 3));
+//
+//            }
+//
+//            sc.nextLine();
+//            System.out.println("Would you like to continue:  ");
+//            yesNo = sc.nextLine();
+//
+//        }while (yesNo.equalsIgnoreCase("Y"));
 
-    if ((grade >= 88) && (grade <= 100)) {
-        System.out.println("A");
-    } else if
-            ((grade >= 80) && (grade <= 87)) {
-        System.out.println("B");
-    } else if
-            ((grade >= 67) && (grade <= 79)) {
-        System.out.println("C");
-    } else if
-            ((grade >= 60) && (grade <= 66)) {
-        System.out.println("D");
-    } else {
-//        ((grade >= 0) && (grade <= 59))
-        System.out.println("F");
-    }
-
-    sc.nextLine();
-    System.out.println("Enter another grade?");
-    yesNo = sc.nextLine();
+     // end main
 
 
-    } while (yesNo.equalsIgnoreCase("Y"));
+
+
+
+
+//        int i = 2;
+//        int square = (int) Math.pow(2, 4);
+
+
+
+
+//String yesNo;
+//Scanner sc = new Scanner(System.in);
+//
+//do {
+//    System.out.println("Please enter your grade:  ");
+//    int grade = sc.nextInt();
+//
+//    if ((grade >= 88) && (grade <= 100)) {
+//        System.out.println("A");
+//    } else if
+//            ((grade >= 80) && (grade <= 87)) {
+//        System.out.println("B");
+//    } else if
+//            ((grade >= 67) && (grade <= 79)) {
+//        System.out.println("C");
+//    } else if
+//            ((grade >= 60) && (grade <= 66)) {
+//        System.out.println("D");
+//    } else {
+////        ((grade >= 0) && (grade <= 59))
+//        System.out.println("F");
+//    }
+//
+//    sc.nextLine();
+//    System.out.println("Enter another grade?");
+//    yesNo = sc.nextLine();
+//
+//    } while (yesNo.equalsIgnoreCase("Y"));
+
+
 
 
     }
---
+++ src/stringOperations.java
@@ -0,0 +1,18 @@
+/**
+ * Created by everardosifuentes on 5/11/17.
+ */
+public class stringOperations {
+    public static void main(String[] args) {
+
+        String sentences = "This is some text";
+
+        //strings are immutable
+        sentences = sentences + "some more text";
+
+        System.out.println(sentences    );
+
+
+
+
+    }
+}
