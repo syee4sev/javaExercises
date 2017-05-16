3d6282863004119bb2bad3da480675b81cb02933
everardosifuentes <everardo.sifuentes@sbcglobal.net>
2017-05-16 15:54:52 -0500

    new person class

---
+++ src/OOprogramming/Person.java
@@ -0,0 +1,31 @@
+package OOprogramming;
+
+/**
+ * Created by everardosifuentes on 5/16/17.
+ */
+public class Person {
+
+    private String name;
+
+
+    // returns the person's name
+    public String getName() {
+        return this.name;
+    }
+    // changes the name property to the passed value
+    public void setName(String name){
+
+    }
+    // prints a message to the console using the person's name
+    public void sayHello(){
+
+    }
+
+    //constructor
+    public Person(String name){
+
+
+
+    }
+
+}
