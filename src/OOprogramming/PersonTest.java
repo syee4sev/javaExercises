bd92001c850a35416dfb30b8e7e1417d22f984f0
everardosifuentes <everardo.sifuentes@sbcglobal.net>
2017-05-16 15:55:31 -0500

    new person class

---
+++ src/OOprogramming/PersonTest.java
@@ -0,0 +1,15 @@
+package OOprogramming;
+
+/**
+ * Created by everardosifuentes on 5/16/17.
+ */
+public class PersonTest {
+
+    public static void main(String[] args) {
+
+        Person pers = new Person();
+        pers.getName();
+
+    }
+
+}
